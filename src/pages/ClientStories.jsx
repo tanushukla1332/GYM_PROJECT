f108ce92060313904051005d75365dee99512ba7
subhadippaul2311 <subhadippaul2001@gmail.com>
2023-04-18 21:03:31 +0530

    clientstories content update

--- src/pages/ClientStories.jsx
+++ src/pages/ClientStories.jsx
@@ -1,20 +1,61 @@
 import React from "react";
+import style from "./ClientStories.module.css"
+
 
 export default function ClientStories() {
+  const clients=[
+    {
+        image:"https://www.grynow.in/assets/blog/top-indian-fitness-influencers/ranveer.webp",
+        name: "Ranveer Allahbadia",
+        description: "An entrepreneur, he is also into men’s style and fitness program. Ranveer, after having undergone a gall bladder surgery at the age of 16 and two more surgeries two years later, he consciously decided to embrace a fitness regime so that he could maintain a healthy lifestyle."
+    },
+    {
+        image:"https://www.grynow.in/assets/blog/top-indian-fitness-influencers/shweta-mehta.webp",
+        name: "Shweta Mehta",
+        description: "Winner of Indian Show, Roadies (Season 15), Shweta is a software engineer but today is a successful bikini athlete and fitness enthusiast and is extremely popular in her online community for her bodyweight trainings. Apart from winning Roadies in 2017, she has also won the Jerai Women’s Physique in 2016 and represented the country at the Asian Bodybuilding Championship in 2016.",
+    },
+    {
+        image:"https://www.grynow.in/assets/blog/top-indian-fitness-influencers/prince.webp",
+        name: "Prince Narula",
+        description: "Winner of Splitsvilla 8, Roadies X2 and Big Boss 9, Price Narula is a classic example of a commoner becoming a celebrity. He is a model and an actor now and very active on social media.",
+    },
+    {
+        image:"https://www.grynow.in/assets/blog/top-indian-fitness-influencers/yasmin.webp",
+        name: "Yasmin Karachiwala",
+        description: "Yasmin is a certified Pilates trainer and instructor. She also owns the Yasmin’s Body Image. Started more than two decades back it was one-of-its-kind fitness class with emphasis on aerobics and palates. She is also renowned as the fitness trainer to Bollywood actress Katrina Kaif and Deepika Padukone."
+    },
+    {
+        image:"https://www.grynow.in/assets/blog/top-indian-fitness-influencers/banji.webp",
+        name: "Bani J.",
+        description: "Her real name is Bani J. She is a muscular woman – covered her entire body with tattoos. She shares her workout sessions and the way to build a body to die for. She also shares her healthy diet and follows a strict gym training; thereby becoming a diva for her followers"
+    },
+    {
+        image:"https://upload.wikimedia.org/wikipedia/commons/b/b0/SangramChougule.png",
+        name: "Saangram Chougule",
+        description:"Sangram Chougule is a 40-year-old body builder from Kolhapur, Maharashtra. Sangram has won both the Mr. Maharashtra title and Mr. India title 5 times each. He also won the Mr. Universe title in 2012 and the Mr. World title in 2014. He is the founder of Physc Gym."
+    }
+     ]
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>EVERY WOMAN HAS A STORY</h1>
-      <h3 style={{margin:'70px', textAlign:'center'}}>
-        Here at Four Friends Fitness, we like to highlight the stories of our
-        clients and their success on their health and fitness journeys. Each
-        client is an integral part of our success at the Four Friends Studio.
-        These clients are at the heart of everything we do and we try our best
-        to make a difference in their lives. We applaud these brave women for
-        finishing strong and crushing their health and fitness goals. These
-        voices and stories from the frontlines have been compiled to help you
-        find the motivation to initiate that change you need to attain physical
-        fitness and a healthy lifestyle.
-      </h3>
+      <div className={style.cardDetails} >
+            {clients.map((ele)=>(
+                <div className={style.card} >
+                 
+                    <img className={style.img}  src={ele.image} alt=""/>
+                   
+                    
+                    <h1 className={style.name}>Name :- {ele.name}</h1>
+                    
+                  
+                    <span className={style.description}>Description :-{ele.description}</span>
+                    
+                </div>
+
+            ))}
+        </div>
     </div>
+    
   );
 }
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,31 +3,47 @@ import Hom from "./Home.module.css";
 
 export default function Home() {
   const [image, setImage] = useState(0);
+  const[text,setText]=useState(0)
 
   const Images=['https://cdn.pixabay.com/photo/2015/07/02/10/22/training-828726_1280.jpg', 
 'https://cdn.pixabay.com/photo/2016/11/22/22/24/adult-1850925_1280.jpg',
-'https://cdn.pixabay.com/photo/2015/07/02/10/27/training-828764_1280.jpg',
+'https://media.istockphoto.com/id/1279902517/photo/be-as-strong-as-you-were-born-to-be.jpg?s=612x612&w=0&k=20&c=VL1HmKItYHiZkUguh-Fsa1W1qccUuWoUQBNLzHqcbns=',
 'https://cdn.pixabay.com/photo/2018/04/05/17/21/kettlebell-3293475_1280.jpg',
 'https://cdn.pixabay.com/photo/2014/12/03/19/05/boxing-555735_1280.jpg'
 ]
+const Text=["You are stronger than you think.  Enhance your health and fitness potential with personalized training designed for women.",
+ "Success usually comes to those who are too busy to be looking for it.", "If you want something you’ve never had, you must be willing to do something you’ve never done.", 
+ "Things work out best for those who make the best of how things work out",
+ "Success is walking from failure to failure with no loss of enthusiasm"]
 
   useEffect(() => {
     const interval = setInterval(() => {
       setImage((image) => (image + 1) % Images.length);
     }, 2000);
-
+    
     return () => {
       clearInterval(interval); // Cleanup the interval on unmount
     };
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText((text) => (text + 1) % Text.length);
+    }, 2000);
+    
+    return () => {
+      clearInterval(interval); // Cleanup the interval on unmount
+    };
+  }, []);
+
+
+
   return (
     <div>
       <section className={Hom.main_file}>
         <div className={Hom.heading}>
-          <h1>
-            You are stronger than you think.  Enhance your health and fitness 
-            potential with personalized training designed for women.
+          <h1>{Text[text]}
+            
           </h1>
         </div>
         <div>
