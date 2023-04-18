5e6ffaa3d5f1dd1f47c925f48de55c7664fc6022
rapstar-surya <surajchaudhari357@gmail.com>
2023-04-18 15:09:38 +0530

    first changes made on second day of project 1st

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import  Hom from './Home.module.css'
+import React, { useEffect, useState } from "react";
+import Hom from "./Home.module.css";
 
 export default function Home() {
-    return (
-        <div>
-            <section className={Hom.main_file}> 
-            <div className={Hom.heading}>
-                <h1 >You are stronger than you think.
-                        Enhance your health and fitness potential with personalized training designed for women.</h1>
-                </div>
-                <div>
-                    <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2021-12/211208-working-out-stock-mn-1310-55e1c7.jpg" alt=""  style={{opacity:"0.5" }}/>
-                
-                </div>
-                          
-        
-            </section>
+  const [image, setImage] = useState(0);
 
+  const Images=['https://cdn.pixabay.com/photo/2015/07/02/10/22/training-828726_1280.jpg', 
+'https://cdn.pixabay.com/photo/2016/11/22/22/24/adult-1850925_1280.jpg',
+'https://cdn.pixabay.com/photo/2015/07/02/10/27/training-828764_1280.jpg',
+'https://cdn.pixabay.com/photo/2018/04/05/17/21/kettlebell-3293475_1280.jpg',
+'https://cdn.pixabay.com/photo/2014/12/03/19/05/boxing-555735_1280.jpg'
+]
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImage((image) => (image + 1) % Images.length);
+    }, 2000);
+
+    return () => {
+      clearInterval(interval); // Cleanup the interval on unmount
+    };
+  }, []);
+
+  return (
+    <div>
+      <section className={Hom.main_file}>
+        <div className={Hom.heading}>
+          <h1>
+            You are stronger than you think.  Enhance your health and fitness 
+            potential with personalized training designed for women.
+          </h1>
         </div>
-    )
+        <div>
+          <img src={Images[image]} alt="" className={Hom.img}/> {/* Update image source to use the correct array indexing */}
+        </div>
+      </section>
+    </div>
+  );
 }
+
